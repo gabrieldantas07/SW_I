f7579ef24cc4be1ab6453b514f98864b575b9e55
Gabriel Dantas <100931688+gabrieldantas07@users.noreply.github.com>
2023-03-14 11:00:48 -0300

    Add files via upload

---
+++ ExercicioAluno/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ExercicioAluno;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Alunos aluno1 = new Alunos();
+        aluno1.nome = "Renan Silva";
+        aluno1.nota1 = 4.3;
+        aluno1.nota2 = 7.8;
+
+        aluno1.mensagem();
+
+        Alunos aluno2 = new Alunos();
+        aluno2.nome = "Ana Fontes";
+        aluno2.nota1 = 4;
+        aluno2.nota2 = 5;
+
+        aluno2.mensagem();
+
+
+    }
+}

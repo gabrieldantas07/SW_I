00f12ac5e47eb57286919d8b01072107d701b9b5
Gabriel Dantas <100931688+gabrieldantas07@users.noreply.github.com>
2023-03-28 11:02:18 -0300

    Projeto Pessoa Exemplo

---
+++ projeto_pessoa/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace projeto_pessoa;
+class Prograim
+{
+    static void Main(string[] args)
+    {
+        Pessoa p1 = new Pessoa();
+        p1.InsereNome("Gabriel");
+        //p1.InsereSobreNome("Dantas");
+        p1.InsereIdade(16);
+
+        p1.MostraDados();
+    }
+}
---
+++ projeto_pessoa/pessoa.cs
@@ -0,0 +1,38 @@
+namespace projeto_pessoa;   
+class Pessoa
+{
+    public Pessoa(){
+        this.Nome = "Não Informado";
+        this.SobreNome = "Não Informado";
+        this.Idade = 0;
+
+    }
+
+    public Pessoa(string nome, int idade){
+        this.Nome = nome;
+        this.Idade = idade;
+        this.SobreNome = "Não Informado";
+    }
+
+    private string? Nome {get; set;}
+    private string? SobreNome {get; set;}
+    private int Idade {get; set;}
+
+    public void InsereNome(string nome){
+        this.Nome = nome;
+    }
+
+    public void InsereSobreNome(string sobrenome){
+        this.SobreNome = sobrenome;
+    }
+
+    public void InsereIdade(int idade){
+        this.Idade = idade;
+    }
+
+    public void MostraDados(){
+        Console.WriteLine("Nome: " + this.Nome);
+        Console.WriteLine("Sobrenome: " + this.SobreNome);
+        Console.WriteLine("Idade: " + this.Idade);
+    }
+}
